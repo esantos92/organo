daaa66270f9fce679e789c063ad903f83b8a811a
Everton Santos <everton.santos@redealumni.com>
2022-12-12 14:03:58 -0300

    nem tudo são flores - aula 02

--- src/components/Time/index.js
+++ src/components/Time/index.js
@@ -11,7 +11,7 @@ const Time = (props) => {
     >
       <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
       <div className='colaboradores'>
-        {props.colaboradores.map( colaborador => <Colaborador nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem}/> )}
+        {props.colaboradores.map( colaborador => <Colaborador key={colaborador.nome} corDeFundo={props.corPrimaria} nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem}/> )}
       </div>
     </section>
     : ''
