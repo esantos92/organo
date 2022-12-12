52a58ef34b4980f6efc29ecfc2a4d1bc28606369
Everton Santos <everton.santos@redealumni.com>
2022-12-12 11:42:37 -0300

    escondendo times vazios - aula 07

--- src/components/Time/index.js
+++ src/components/Time/index.js
@@ -3,7 +3,7 @@ import './Time.css'
 
 const Time = (props) => {
   return (
-    <section
+    (props.colaboradores.length > 0) ? <section
       className='time'
       style={{
         backgroundColor: props.corSecundaria
@@ -11,9 +11,10 @@ const Time = (props) => {
     >
       <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
       <div className='colaboradores'>
-      {props.colaboradores.map( colaborador => <Colaborador nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem}/> )}
+        {props.colaboradores.map( colaborador => <Colaborador nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem}/> )}
       </div>
     </section>
+    : ''
   )
 }
 
